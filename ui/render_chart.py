955edcb4afb2e0ed162d2cc457057fbb32a4c04d
Andy Cowley <andy.m.cowley@googlemail.com>
2018-09-29 13:30:57 +0100

    Fixed Cron Job Syntax

--- ui/render_chart.py
+++ ui/render_chart.py
@@ -30,6 +30,31 @@ def chart():
                             min=10500
                             )
 
+def newmethod282():
+    def chart():
+        labels = []
+        values = []
+
+        api = os.environ['API']
+        url = "%s/api/history" % api
+        payload = requests.get(url)
+        data = json.loads(json.loads(payload.text))
+
+        for entry in data['history']:
+            label = entry['update_time']
+            value = entry['total_cashout']
+            labels.append(label)
+            values.append(value)
+
+        return render_template( 
+                                'chart.html', 
+                                title="What are your shares worth now?", 
+                                labels=labels[::-1], 
+                                values=values[::-1],
+                                max=15000,
+                                min=10500
+                                )
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
     # print(type(data), data)
